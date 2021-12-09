968e5082ac3466ef3907746cb46bce8f339cb148
Mohammed Afwan <afwan.md2@gmail.com>
2021-12-09 16:12:26 +0100

    Committing Search API

--- src/VerticalPrototype/Server/app.js
+++ src/VerticalPrototype/Server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const dbConnection = require("./db");
 
 app.use(cors());
 app.use(express.json());
@@ -9,6 +10,15 @@ app.use(express.json());
 
 app.get('/test', (req, res) => {
     res.send("Hello World");
+});
+
+app.get("/api/tutors/get", (req, res) => {
+    const {name,subject} = req.body;
+    let sql = `SELECT * FROM tutors WHERE name = "${name}" or subject = "${subject}"`
+    dbConnection.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
 });
 
 app.listen('4000', () => {

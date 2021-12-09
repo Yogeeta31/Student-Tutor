bc2500bfb9f0e5acf99db8b6422270c0fdf4e209
Yogeeta31 <yogeeta.sharma@informatik.hs-fulda.de>
2021-12-09 16:36:28 +0100

    Scripts

---
+++ script/removeTutors.js
@@ -0,0 +1,39 @@
+const mysql = require('mysql'); 
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "mentorme-test",
+  });
+  
+  con.connect((err) => {
+    if (err)
+    {
+        console.log('Error in Connection');
+    }
+    else
+    {
+        console.log("Successful Connection");
+        removeTutors();
+        con.end((err)=>{
+            if (err) {
+                console.log('error:' + err.message);
+            }
+            else
+            {
+                console.log("Connection Closed");
+                process.exit();
+            }
+        });
+    }
+  });
+
+const removeTutors = ()=>{
+    var sql = 'DELETE FROM tutor';
+        con.query(sql,(err, result) => {
+            if (err){
+                console.log(err)
+            }
+        })
+}
--- script/script/removeTutors.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql'); 
-const con = require('../app.js');
-
-const removeTutors = (req,res)=>{
-    console.log("what is down");
-}
-
-module.exports = removeTutors

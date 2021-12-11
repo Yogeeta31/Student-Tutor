7ffc048ca84390ef652b7c7c36b6afe92df01b29
Mohammed Afwan <afwan.md2@gmail.com>
2021-12-11 12:32:08 +0100

    Committing Search API

--- src/VerticalPrototype/Server/app.js
+++ src/VerticalPrototype/Server/app.js
@@ -14,7 +14,7 @@ app.get('/test', (req, res) => {
 
 app.get("/api/tutors/", (req, res) => {
     const {first_name, last_name,subject_name} = req.body;
-    let sql = `SELECT * FROM tutor INNER JOIN subject ON (tutor.SUBJECT_ID=subject.SUBJECT_ID) WHERE FIRST_NAME LIKE "%${first_name}%" OR LAST_NAME LIKE "%${last_name}%" OR SUBJECT_NAME LIKE "%${subject_name}%"`;
+    let sql = `SELECT * FROM User INNER JOIN tutor ON (tutor.USER_ID=user.USER_ID) INNER JOIN subject ON (subject.SUBJECT_ID = tutor.SUBJECT_ID) WHERE FIRST_NAME LIKE "%${first_name}%" OR LAST_NAME LIKE "%${last_name}%" OR SUBJECT_NAME LIKE "%${subject_name}%"`;
     dbConnection.query(sql, (err, result) => {
         if (err) throw err;
         res.send(result);

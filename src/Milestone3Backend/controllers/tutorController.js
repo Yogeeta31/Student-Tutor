cdc6b7a4e1d844cf87a566bec3eea5e43c540d89
Mohammed Afwan <afwan.md2@gmail.com>
2022-01-06 22:21:36 +0100

    Updated the getTutor Details API

--- src/Milestone3Backend/controllers/tutorController.js
+++ src/Milestone3Backend/controllers/tutorController.js
@@ -2,7 +2,8 @@ const dbConnection = require("../db");
 var _ = require("underscore");
 module.exports.getTutorDetails = (req, res) => {
         let { userID } = req.query;
-        let sql = `SELECT * FROM USER, TUTOR WHERE USER.USER_ID = TUTOR.USER_ID AND USER.USER_ID = %${userID}%`;
+        let sql = `SELECT USER.NAME, USER.MOBILE_NO, USER.EMAIL, USER.IMAGE, USER.LAST_LOGIN, TUTOR.SUBJECT_ID, TUTOR.PRICE, TUTOR.IS_APPROVED, TUTOR.CV FROM USER INNER JOIN TUTOR ON (TUTOR.USER_ID=USER.USER_ID) WHERE USER.USER_ID = "${userID}"`;
+
         dbConnection.query(sql, (err, result) => {
             if (err) throw err;
             res.send(result);

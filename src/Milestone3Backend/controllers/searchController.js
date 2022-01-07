e3728813b5d0e818ef2386825364bae6a9fe5a34
omarelhedaby <omarelhedabi@gmail.com>
2022-01-07 20:59:15 +0100

    Search Tutor Changes

--- src/Milestone3Backend/controllers/searchController.js
+++ src/Milestone3Backend/controllers/searchController.js
@@ -6,7 +6,7 @@ module.exports.search_tutor_get = (req, res) => {
     searchTerm = "";
   }
   if (Object.keys(req.query).length === 0) {
-    let sql = `SELECT USER.USER_ID, USER.NAME, SUBJECT.SUBJECT_NAME,AVG(REVIEWS.RATING) AS AVERAGE_RATING, SUBJECT.PRICE FROM USER INNER JOIN TUTOR ON (TUTOR.USER_ID=USER.USER_ID) INNER JOIN SUBJECT ON (SUBJECT.USER_ID= TUTOR.USER_ID) INNER JOIN REVIEWS ON (TUTOR.USER_ID=REVIEWS.TO_USER_ID) GROUP BY TUTOR.USER_ID`;
+    let sql = `SELECT u.NAME,t.USER_ID , s.SUBJECT_NAME, s.PRICE , AVG(r.RATING) AS AVERAGE_RATING , s.PRICE , s.SUBJECT_ID FROM TUTOR t INNER JOIN  SUBJECT s ON (t.USER_ID = s.USER_ID) INNER JOIN USER u ON (t.USER_ID = u.USER_ID) INNER JOIN REVIEWS r ON (u.USER_ID = r.TO_USER_ID AND r.SUBJECT_ID = s.SUBJECT_ID) GROUP BY s.SUBJECT_NAME`;
     dbConnection.query(sql, (err, result) => {
       if (err) throw err;
       res.send(result);

8d04987b19678b8ca68cd8d7e531f576fae0482a
Bibek Gaihre <bibekgaihre123@example.com>
2022-01-07 15:14:57 +0100

    MS3 changes in signup

--- src/Milestone3Backend/app.js
+++ src/Milestone3Backend/app.js
@@ -4,6 +4,7 @@ const app = express();
 const dbConnection = require("./db");
 const db = require("./db");
 const indexRoute = require("./routes/index");
+const http = require("http");
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,21 @@ app.use(express.static("public"));
 
 app.use("/", indexRoute);
 
-app.listen("4000", () => {
+let Server = http.createServer(app);
+const io = require("socket.io")(Server, {
+  cors: {
+    origin: `http://localhost:${process.env.FRONTEND_PORT}`,
+    methods: ["GET", "POST"],
+    credentials: true,
+    transports: ["websocket", "polling"],
+  },
+  allowEIO3: true,
+});
+
+io.on("connection", (socket) => {
+  socket.on("sendmessage", async (data) => {});
+});
+
+Server.listen(4000, () => {
   console.log("Server started");
 });

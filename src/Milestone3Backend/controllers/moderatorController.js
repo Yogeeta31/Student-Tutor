8c126d7cdd41e7a101360f2ec464f5d52d36d15c
Ankit-HS <93345461+Ankit-HS@users.noreply.github.com>
2022-01-23 12:01:20 +0100

    ST-25:Ban User functionality

--- src/Milestone3Backend/controllers/moderatorController.js
+++ src/Milestone3Backend/controllers/moderatorController.js
@@ -1,4 +1,5 @@
 const dbConnection = require("../db");
+const util = require("util");
 module.exports.approve_tutor_patch = (req, res) => {
   const { tutorId, isApprove } = req.body;
   const approveTutor = `UPDATE TUTOR
@@ -38,6 +39,39 @@ module.exports.rejectProfileWithReason = (req, res) => {
   let {reason, senderId, receiverId } = req.body;
   const postRejectMessage = `INSERT INTO REJECT_REASON ( REASON, SENDER_ID,RECEIVER_ID,TIME_SENT ) VALUES ("${reason}",${senderId},${receiverId},"${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')}")`;
   dbConnection.query(postRejectMessage, async (err, result) => {
+    if (err) {
+      return res.status(400).json(err);
+    }
+    res.status(200).json(result);
+  });
+};
+
+module.exports.banProfile = async (req, res) => {
+  let {reason, moderatorId, userId } = req.body;
+  const dbPromise = util.promisify(dbConnection.query).bind(dbConnection);
+  const updateUser = `UPDATE User
+                      SET HAS_PERMISSION = 0
+                      WHERE USER_ID = ${userId}`;
+  const postRejectMessage = `INSERT INTO 
+                             BANNED_USER ( REASON, SENDER_ID,RECEIVER_ID,TIME_SENT ) 
+                             VALUES ("${reason}",${moderatorId},${userId},"${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')}")`;
+    try {
+      result = await dbPromise(updateUser);
+    } catch (err) {
+      throw err;
+    }
+
+    try {
+      result = await dbPromise(postRejectMessage);
+    } catch (err) {
+      throw err;
+    }
+    res.status(200).json(result);
+};
+
+module.exports.getEnrolledStudents = (req, res) => {
+  const list = `SELECT * FROM User WHERE User.ROLE_ID = 1`;
+  dbConnection.query(list, async (err, result) => {
     if (err) {
       return res.status(400).json(err);
     }

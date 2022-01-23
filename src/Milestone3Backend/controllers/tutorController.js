d636daf1ced633737bc1ef2b6586513742be3202
omarelhedaby <47994224+omarelhedaby@users.noreply.github.com>
2022-01-23 21:02:45 +0100

    Update tutorController.js

--- src/Milestone3Backend/controllers/tutorController.js
+++ src/Milestone3Backend/controllers/tutorController.js
@@ -31,7 +31,7 @@ module.exports.getTutorDetails = async (req, res) => {
   var tutor = JSON.parse(JSON.stringify(result[0]));
 
   tutor["subjects"] = [];
-  let sqlSubectQuery = `SELECT s.SUBJECT_NAME, s.PRICE , AVG(r.RATING) AS AVERAGE_RATING FROM TUTOR t  INNER JOIN SUBJECT s  ON (t.USER_ID = s.USER_ID) INNER JOIN REVIEWS r ON (t.USER_ID = r.TO_USER_ID AND r.SUBJECT_ID = s.SUBJECT_ID) WHERE t.USER_ID = ${user_id} GROUP BY s.SUBJECT_NAME`;
+  let sqlSubectQuery = `SELECT s.SUBJECT_ID,s.SUBJECT_NAME, s.PRICE , AVG(r.RATING) AS AVERAGE_RATING FROM TUTOR t  INNER JOIN SUBJECT s  ON (t.USER_ID = s.USER_ID) INNER JOIN REVIEWS r ON (t.USER_ID = r.TO_USER_ID AND r.SUBJECT_ID = s.SUBJECT_ID) WHERE t.USER_ID = ${user_id} GROUP BY s.SUBJECT_NAME`;
   let subjects = null;
   try {
     subjects = await dbPromise(sqlSubectQuery);
@@ -41,7 +41,7 @@ module.exports.getTutorDetails = async (req, res) => {
   tutor["subjects"] = JSON.parse(JSON.stringify(subjects));
 
   tutor["reviews"] = [];
-  let sqlReviewQuery = `SELECT r.REVIEW, r.RATING FROM TUTOR t  INNER JOIN REVIEWS r  ON (t.USER_ID = r.TO_USER_ID) WHERE t.USER_ID = ${user_id} ORDER BY r.RATING DESC LIMIT 3`;
+  let sqlReviewQuery = `SELECT r.REVIEW, r.RATING FROM TUTOR t  INNER JOIN REVIEWS r  ON (t.USER_ID = r.TO_USER_ID) WHERE t.USER_ID = ${user_id} ORDER BY r.RATING DESC`;
   let reviews = null;
   try {
     reviews = await dbPromise(sqlReviewQuery);
